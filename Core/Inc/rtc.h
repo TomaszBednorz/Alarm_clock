6f0217646297ef9cd54de30097a86765817f9586
Tomasz Bednorz <tomekbed99@gmail.com>
2021-08-05 13:38:29 +0200

    rtc files initialization

---
+++ Core/Inc/rtc.h
@@ -0,0 +1,13 @@
+/*
+ * rtc.h
+ *
+ *  Created on: Aug 5, 2021
+ *      Author: tomek
+ */
+
+#ifndef INC_RTC_H_
+#define INC_RTC_H_
+
+
+
+#endif /* INC_RTC_H_ */
---
+++ Core/Src/rtc.c
@@ -0,0 +1,8 @@
+/*
+ * rtc.c
+ *
+ *  Created on: Aug 5, 2021
+ *      Author: tomek
+ */
+
+
